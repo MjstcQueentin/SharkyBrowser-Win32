00bf6b1123f04dcc8520f5160214491317749451
Quentin Pugeat <contact@quentinpugeat.fr>
2025-10-26 18:28:49 +0100

    N'ajouter que les pages nouvellement chargées dans l'historique

--- SharkyBrowser/SharkyWeb/SharkyWebView.cs
+++ SharkyBrowser/SharkyWeb/SharkyWebView.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Imaging;
+using Microsoft.Web.WebView2.Core;
 using SharkyBrowser.SharkyFilter;
 using SharkyBrowser.SharkyFilter.FilterCategories;
 using SharkyBrowser.SharkySettings;
@@ -19,17 +20,10 @@ namespace SharkyBrowser.SharkyWeb
         {
             CoreWebView2Initialized += SharkyWebView_CoreWebView2Initialized;
             NavigationStarting += SharkyWebView_NavigationStarting;
-            NavigationCompleted += SharkyWebView_NavigationCompleted;
         }
 
-        private void SharkyWebView_NavigationStarting(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs args)
+        private void SharkyWebView_NavigationStarting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
         {
-            CurrentPage = new()
-            {
-                Name = args.Uri,
-                Uri = new(args.Uri)
-            };
-
             // Annuler la navigation si l'URI correspond à un filtre de contenu
             SharkyFilteredResource filteredResource = SharkyFilterController.TestUri(new(args.Uri), SharkyFilteringContext.Navigating);
             if (filteredResource.HasMatched)
@@ -38,45 +32,57 @@ namespace SharkyBrowser.SharkyWeb
                 args.Cancel = true;
                 return;
             }
-        }
 
-        private void SharkyWebView_NavigationCompleted(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs args)
-        {
-            SharkyUserDatabase.Instance.InsertResource("history", CurrentPage);
+            if (args.NavigationKind == CoreWebView2NavigationKind.NewDocument)
+            {
+                CurrentPage = new()
+                {
+                    Name = args.Uri,
+                    Uri = new(args.Uri)
+                };
+
+                SharkyUserDatabase.Instance.InsertResource("history", CurrentPage);
+            }
+            else if (args.NavigationKind == CoreWebView2NavigationKind.BackOrForward)
+            {
+                CurrentPage = null;
+            }
         }
 
         private void SharkyWebView_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs args)
         {
-            sender.CoreWebView2.AddWebResourceRequestedFilter("http://*", Microsoft.Web.WebView2.Core.CoreWebView2WebResourceContext.All);
-            sender.CoreWebView2.AddWebResourceRequestedFilter("https://*", Microsoft.Web.WebView2.Core.CoreWebView2WebResourceContext.All);
+            sender.CoreWebView2.AddWebResourceRequestedFilter("http://*", CoreWebView2WebResourceContext.All);
+            sender.CoreWebView2.AddWebResourceRequestedFilter("https://*", CoreWebView2WebResourceContext.All);
             sender.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
             sender.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
             sender.CoreWebView2.FaviconChanged += CoreWebView2_FaviconChanged;
         }
 
-        private void CoreWebView2_FaviconChanged(Microsoft.Web.WebView2.Core.CoreWebView2 sender, object args)
+        private void CoreWebView2_FaviconChanged(CoreWebView2 sender, object args)
         {
             if (string.IsNullOrEmpty(sender.FaviconUri)) return;
+            if (CurrentPage is null) return;
 
             var logo = new BitmapImage
             {
-                UriSource = new System.Uri(sender.FaviconUri)
+                UriSource = new(sender.FaviconUri)
             };
 
             CurrentPage.Icon = logo;
             SharkyUserDatabase.Instance.UpdateResource("history", CurrentPage);
         }
 
-        private void CoreWebView2_DocumentTitleChanged(Microsoft.Web.WebView2.Core.CoreWebView2 sender, object args)
+        private void CoreWebView2_DocumentTitleChanged(CoreWebView2 sender, object args)
         {
+            if (CurrentPage is null) return;
             CurrentPage.Name = sender.DocumentTitle;
             SharkyUserDatabase.Instance.UpdateResource("history", CurrentPage);
         }
 
-        private void CoreWebView2_WebResourceRequested(Microsoft.Web.WebView2.Core.CoreWebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2WebResourceRequestedEventArgs args)
+        private void CoreWebView2_WebResourceRequested(CoreWebView2 sender, CoreWebView2WebResourceRequestedEventArgs args)
         {
             // Si l'URI correspond à un filtre de contenu, annuler la requête en falsifiant la réponse
-            if (args.ResourceContext != Microsoft.Web.WebView2.Core.CoreWebView2WebResourceContext.Document)
+            if (args.ResourceContext != CoreWebView2WebResourceContext.Document)
             {
                 SharkyFilteredResource filteredResource = SharkyFilterController.TestUri(new(args.Request.Uri), SharkyFilteringContext.FetchingResource);
                 if (filteredResource.HasMatched)
